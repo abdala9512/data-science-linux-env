01a36c0f5d4df86c23ae1ac32e9b67352eb40dbd
abdala9512 <miguel.arquez12@gmail.com>
2020-11-02 11:33:31 -0500

    DS Config CentOS

---
+++ ds_config_centos.sh
@@ -0,0 +1,36 @@
+
+# This bash script install python 3.7, jupyter notebook, airflow, docker and git 
+# Python - jupyter - standard modules
+sudo yum install  -y python37
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+sudo python3 get-pip.py
+pip install --user jupyter
+
+
+# Docker
+sudo yum install -y python3-pip python3 python3-setuptools
+sudo yum update -y
+sudo amazon-linux-extras install docker
+
+# Git
+sudo yum install git
+
+# GCP python modules
+curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-255.0.0-linux-x86_64.tar.gz
+tar zxvf google-cloud-sdk-255.0.0-linux-x86_64.tar.gz
+./google-cloud-sdk/install.s
+
+
+# Python modules
+pip install pandas --user
+pip install pandas --user
+pip install sklearn -- user
+pip install airflow --user
+pip install --user pandas_gbq
+pip install matplotlib --user
+pip install --user google-cloud-bigquery
+
+
+# Airflow
+export  AIRFLOW_HOME=~/airflow
+pip install apache-airflow
