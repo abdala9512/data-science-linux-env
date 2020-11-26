28ae15e7141ef07cde86c9ebe523a89042494eb8
Ubuntu <ubuntu@ip-172-31-41-146.ec2.internal>
2020-11-26 03:23:45 +0000

    Multi Distribution setup

--- ds_simple_setup.sh
+++ ds_simple_setup.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+DISTRIBUTION=${1:-ubuntu}
+
+install_python_libraries(){
+    echo "Installin python libraries"
+
+    pip install pandas --user
+    pip install sklearn -- user
+    pip install airflow --user
+    pip install --user pandas_gbq
+    pip install matplotlib --user
+    pip install --user google-cloud-bigquery
+    pip install tensorflow
+    
+
+}
 
 echo "Data Science setup. The following tools will be installed:"
 echo -e "\n1. Python V. 3.8.5 "
@@ -7,6 +22,26 @@ echo -e "\n2. Git Latest version"
 echo -e "\n3. Docker Latest version"
 
 
+if [ $DISTRIBUTION = "ubuntu" ]; then
+#    sudo apt update
+#    sudo apt upgrade
+    echo "running ubuntu configuraiton.."
+else
+#    sudo yumn update -y
+    echo " running centos configurations"
+    sudo yum -y update
+    sudo yum in
+
+    # git
+    sudo yum install git
+    
+    # Python setup
+
+    install_python_libraries
+    
+fi
+
+: '
 # This bash script install python 3.7, jupyter notebook, airflow, docker and git 
 # Python - jupyter - standard modules
 sudo yum install  -y python37
@@ -28,17 +63,7 @@ curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-
 tar zxvf google-cloud-sdk-255.0.0-linux-x86_64.tar.gz
 ./google-cloud-sdk/install.s
 
-
-# Python modules
-pip install pandas --user
-pip install pandas --user
-pip install sklearn -- user
-pip install airflow --user
-pip install --user pandas_gbq
-pip install matplotlib --user
-pip install --user google-cloud-bigquery
-
-
 # Airflow
 export  AIRFLOW_HOME=~/airflow
 pip install apache-airflow
+'
