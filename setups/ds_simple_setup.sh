3816f729c2fbc2577840582827245526b4a41d0a
abdala9512 <miguel.arquez12@gmail.com>
2020-11-27 03:32:10 +0000

    Python setup fix

--- setups/ds_simple_setup.sh
+++ setups/ds_simple_setup.sh
@@ -1,39 +1,57 @@
 #!/bin/bash
 
+
+CURRENT_PATH=$(pwd)
 DISTRIBUTION=${1:-ubuntu}
 SPARK_CONFIG=${2:-false}
 
 install_python_libraries(){
-    echo "Installin python libraries"
+    echo "Installing python libraries..."
 
     # Download env_requirements
-    wget https://raw.githubusercontent.com/abdala9512/data-science-linux-env/main/env_requirements.txt
+    if [ ! -f ./env_requirements.txt ]; then
+        wget https://raw.githubusercontent.com/abdala9512/data-science-linux-env/main/env_requirements.txt
+    fi
 
     for lib in $(env_requirements.txt);
     do
-        pip3 intall $lib --user
+        pip3 install $lib --user
     done
     
     rm env_requirements.txt
-
-
 }
+
+message(){
 
 echo "Data Science setup. The following tools will be installed:"
 echo -e "\n1. Python V. 3.8.6 "
 echo -e "\n2. Git Latest version"
 echo -e "\n3. Docker Latest version"
 
+}
+
 
 if [ $DISTRIBUTION = "ubuntu" ]; then
-#    sudo apt update
-#    sudo apt upgrade
-    echo "running ubuntu configuraiton.."
-    
-    # Docker installation
-    sudo apt-get remove docker docker-engine docker.io containerd runc
-    # Python setup
+       
+    echo "Running Ubuntu configuration.."
+    cd
 
+    message
+    sudo apt update
+    sudo apt upgrade
+    
+    #
+    cd /usr/lib/python3/dist-packages
+    sudo ln -s apt_pkg.cpython-36m-x86_64-linux-gnu.so apt_pkg.so
+    cd 
+
+    # Git 
+    sudo apt install git
+
+    # Docker installation
+    sudo apt install docker.io
+ 
+    # Python setup
     sudo add-apt-repository ppa:deadsnakes/ppa
     sudo apt install python3.8
     sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 10
@@ -41,11 +59,13 @@ if [ $DISTRIBUTION = "ubuntu" ]; then
     echo "alias python='python3'" >> ~/.bashrc
     
     sudo apt install python3-pip
-
     install_python_libraries
+    
+    # Heroku CLI
+    sudo snap install --classic heroku
 
 
-    if [ $SPARK_CONFIG ]; then
+    if [ "$SPARK_CONFIG" = true ]; then
         echo "Spark installation...."
         
         sudo add-apt-repository ppa:openjdk-r/ppa
@@ -63,44 +83,41 @@ if [ $DISTRIBUTION = "ubuntu" ]; then
         # Spark environment variables
 
 
-
     fi
-else
-#    sudo yumn update -y
-    echo " running centos configurations"
+    
+    cd $CURRENT_PATH
+    echo "Ubuntu setup succesfully finished :)"
+
+elif [ $DISTRIBUTION = 'redhat' ]; then
+   
+    echo " running centos configuration..."
+    cd
+    
+    message
     sudo yum -y update
 
     # git
     sudo yum install git
     
     # Python setup
-    sudo yum install -y  python38
+    sudo yum install -y python38
     curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
     sudo python3 get-pip.py
 
+    echo "alias python='python3'" >> ~/.bashrc
     install_python_libraries
 
     export AIRFLOW_HOME=~/airflow
     pip install apache-airflow
-    
+
+    cd $CURRENT_PATH
+else
+    echo "Distribution not recognized"
+    exit 0
 fi
 
 : '
-# This bash script install python 3.7, jupyter notebook, airflow, docker and git 
-# Python - jupyter - standard modules
-sudo yum install  -y python37
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-sudo python3 get-pip.py
-pip install --user jupyter
 
-
-# Docker
-sudo yum install -y python3-pip python3 python3-setuptools
-sudo yum update -y
-sudo amazon-linux-extras install docker
-
-
-# GCP python modules
 curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-255.0.0-linux-x86_64.tar.gz
 tar zxvf google-cloud-sdk-255.0.0-linux-x86_64.tar.gz
 ./google-cloud-sdk/install.s
